4f4e46fbdbd1942602b4a3e4f4c7b73e7c295794
yura <rladbfk0000@naver.com>
2023-07-01 00:26:49 +0900

    feat : 채팅 메세지 받기

--- src/pages/Chatting/index.jsx
+++ src/pages/Chatting/index.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const index = () => {
+  const [messageInput, setMessageInput] = useState('');
+
   const socket = new WebSocket('ws://localhost:8080/ws/chat');
+
   socket.onopen = () => {
     console.log('WebSocket connection opened.');
 
@@ -17,23 +20,24 @@ const index = () => {
 
     // WebSocket에 메시지 전송
     var message = {
-      productId: 'c409c6f7-699e-40f9-bb2a-ccc93e63d927',
+      productUuid: 'c409c6f7-699e-40f9-bb2a-ccc93e63d927',
       type: 'TALK', // ENTER
       roomId: '1',
       sender: '',
-      message: 'ㅎㅇ',
-      // email: 'dayon@gmail.com',
+      message: '',
       time: '',
     };
     socket.send(JSON.stringify(message));
 
     // // API 요청 보내기
-    var email = 'dayon@gmail.com';
-    var productId = 'c409c6f7-699e-40f9-bb2a-ccc93e63d927';
+    const email = 'dayon@gmail.com';
+    const productUuid = 'c409c6f7-699e-40f9-bb2a-ccc93e63d927';
 
     // 방만드는곳
     axios
-      .post(`http://localhost:8080/chat?email=${email}&productId=${productId}`)
+      .post(
+        `http://localhost:8080/chat?email=${email}&productUuid=${productUuid}`,
+      )
       .then((response) => {
         console.log('API response:', response.data);
 
@@ -78,34 +82,38 @@ const index = () => {
   // 대화 메시지 처리 함수
   const handleTalkMessage = (message) => {
     console.log('Talk message received:', message);
-    // 대화 메시지 처리 로직 작성
+
+    // 메세지 받는거 message.message && message.sender 해서 ui에 띄워주기
+    socket.onmessage = function (event) {
+      var receivemessage = JSON.parse(event.data);
+      console.log(receivemessage);
+
+      // 메시지 처리 로직 작성
+      switch (message.type) {
+        case 'TALK':
+          handleTalkMessage(message);
+          break;
+        default:
+          console.log('Unknown message type:', message.type);
+      }
+    };
   };
 
-  const ChatContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  `;
+  const handleSendMessage = () => {
+    // 메시지 전송 로직
+    var message = {
+      productUuid: 'c409c6f7-699e-40f9-bb2a-ccc93e63d927',
+      type: 'TALK', // ENTER
+      roomId: '1',
+      sender: '',
+      time: '',
+      message: messageInput,
+    };
+    socket.send(JSON.stringify(message));
 
-  const ChatMessages = styled.div`
-    flex: 1;
-    overflow-y: auto;
-  `;
-
-  const MessageBubble = styled.div`
-    background-color: #f2f2f2;
-    padding: 10px;
-    margin-bottom: 10px;
-    border-radius: 8px;
-  `;
-
-  const ChatInput = styled.input`
-    padding: 10px;
-    border: none;
-    border-top: 1px solid #ccc;
-    outline: none;
-  `;
+    // 메시지 입력창 초기화
+    setMessageInput('');
+  };
 
   return (
     <ChatContainer>
@@ -114,9 +122,58 @@ const index = () => {
         <MessageBubble>반갑습니다.</MessageBubble>
         {/* 채팅 메시지들을 동적으로 렌더링 */}
       </ChatMessages>
-      <ChatInput type="text" placeholder="메시지를 입력하세요" />
+      <ChatInputContainer>
+        <ChatInput
+          type="text"
+          placeholder="메시지를 입력하세요"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+        />
+        <SendButton onClick={handleSendMessage}>전송</SendButton>
+      </ChatInputContainer>
     </ChatContainer>
   );
 };
+const ChatContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
+const ChatMessages = styled.div`
+  flex: 1;
+  overflow-y: auto;
+`;
+
+const MessageBubble = styled.div`
+  background-color: #f2f2f2;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 8px;
+`;
+
+const ChatInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-top: 1px solid #ccc;
+`;
+
+const ChatInput = styled.input`
+  flex: 1;
+  padding: 8px;
+  border: none;
+  outline: none;
+`;
+
+const SendButton = styled.button`
+  padding: 8px 12px;
+  margin-left: 10px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
 export default index;
