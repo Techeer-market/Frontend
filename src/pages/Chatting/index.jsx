ede516c69e6fdfc9af19a5022f07f78c30739e7f
yura <rladbfk0000@naver.com>
2023-07-01 00:56:16 +0900

    fix : api 요청 한번만 && 내가보낸 메세지 확인

--- src/pages/Chatting/index.jsx
+++ src/pages/Chatting/index.jsx
@@ -1,95 +1,24 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
-const index = () => {
+const Index = () => {
   const [messageInput, setMessageInput] = useState('');
 
-  const socket = new WebSocket('ws://localhost:8080/ws/chat');
-
-  socket.onopen = () => {
-    console.log('WebSocket connection opened.');
-
-    // Response 데이터 받아오기
-    const response = {
-      productUuid: '',
-      // "roomId": randomUUID();
-      name: 'test',
-      sessions: [],
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080/ws/chat');
+    socket.onopen = () => {
+      console.log('WebSocket connection opened.');
     };
 
-    // WebSocket에 메시지 전송
-    var message = {
-      productUuid: 'c409c6f7-699e-40f9-bb2a-ccc93e63d927',
-      type: 'TALK', // ENTER
-      roomId: '1',
-      sender: '',
-      message: '',
-      time: '',
-    };
-    socket.send(JSON.stringify(message));
-
-    // // API 요청 보내기
-    const email = 'dayon@gmail.com';
-    const productUuid = 'c409c6f7-699e-40f9-bb2a-ccc93e63d927';
-
-    // 방만드는곳
-    axios
-      .post(
-        `http://localhost:8080/chat?email=${email}&productUuid=${productUuid}`,
-      )
-      .then((response) => {
-        console.log('API response:', response.data);
-
-        // API 응답 처리 로직 작성
-      })
-      .catch((error) => {
-        console.error('API request error:', error);
-      });
-  };
-
-  socket.onmessage = (event) => {
-    const message = JSON.parse(event.data);
-
-    // 메시지 처리 로직 작성
-    switch (message.type) {
-      // case 'ENTER':
-      //   handleEnterMessage(message);
-      //   break;
-      case 'TALK':
-        handleTalkMessage(message);
-        break;
-      default:
-        console.log('Unknown message type:', message.type);
-    }
-  };
-
-  socket.onclose = (event) => {
-    console.log(
-      'WebSocket connection closed with code:',
-      event.code,
-      'reason:',
-      event.reason,
-    );
-  };
-
-  // 입장 메시지 처리 함수
-  // const handleEnterMessage = (message) => {
-  //   console.log('Enter message received:', message);
-  //   // 입장 메시지 처리 로직 작성
-  // };
-
-  // 대화 메시지 처리 함수
-  const handleTalkMessage = (message) => {
-    console.log('Talk message received:', message);
-
-    // 메세지 받는거 message.message && message.sender 해서 ui에 띄워주기
-    socket.onmessage = function (event) {
-      var receivemessage = JSON.parse(event.data);
-      console.log(receivemessage);
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
 
       // 메시지 처리 로직 작성
       switch (message.type) {
+        // case 'ENTER':
+        //   handleEnterMessage(message);
+        //   break;
         case 'TALK':
           handleTalkMessage(message);
           break;
@@ -97,22 +26,74 @@ const index = () => {
           console.log('Unknown message type:', message.type);
       }
     };
+
+    socket.onclose = (event) => {
+      console.log(
+        'WebSocket connection closed with code:',
+        event.code,
+        'reason:',
+        event.reason,
+      );
+    };
+
+    return () => {
+      // 컴포넌트가 언마운트되면 WebSocket 연결 종료
+      socket.close();
+    };
+  }, []);
+
+  // 대화 메시지 처리 함수
+  const handleTalkMessage = (message) => {
+    console.log('Talk message received:', message);
+
+    // 메세지 받는거 message.message && message.sender 해서 ui에 띄워주기
   };
 
   const handleSendMessage = () => {
+    if (messageInput.trim() === '') {
+      return; // 빈 메시지는 전송하지 않음
+    }
+
     // 메시지 전송 로직
-    var message = {
+    const socket = new WebSocket('ws://localhost:8080/ws/chat');
+    const message = {
       productUuid: 'c409c6f7-699e-40f9-bb2a-ccc93e63d927',
-      type: 'TALK', // ENTER
+      type: 'TALK',
       roomId: '1',
       sender: '',
       time: '',
       message: messageInput,
     };
-    socket.send(JSON.stringify(message));
+    socket.onopen = () => {
+      socket.send(JSON.stringify(message));
+      console.log('Message sent:', message);
+
+      // API 요청
+      const email = 'dayon@gmail.com';
+      const productUuid = 'c409c6f7-699e-40f9-bb2a-ccc93e63d927';
+
+      axios
+        .post(
+          `http://localhost:8080/chat?email=${email}&productUuid=${productUuid}`,
+        )
+        .then((response) => {
+          console.log('API response:', response.data);
+          // API 응답 처리 로직 작성
+        })
+        .catch((error) => {
+          console.error('API request error:', error);
+        });
+    };
 
     // 메시지 입력창 초기화
     setMessageInput('');
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // 엔터키의 기본 동작 방지
+      handleSendMessage(); // 메시지 전송 함수 호출
+    }
   };
 
   return (
@@ -128,12 +109,14 @@ const index = () => {
           placeholder="메시지를 입력하세요"
           value={messageInput}
           onChange={(e) => setMessageInput(e.target.value)}
+          onKeyDown={handleKeyDown} // 엔터 키 이벤트 핸들러 추가
         />
         <SendButton onClick={handleSendMessage}>전송</SendButton>
       </ChatInputContainer>
     </ChatContainer>
   );
 };
+
 const ChatContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -176,4 +159,5 @@ const SendButton = styled.button`
   border-radius: 4px;
   cursor: pointer;
 `;
-export default index;
+
+export default Index;
