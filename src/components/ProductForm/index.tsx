23823df1b8b6c7ce1e4c426bf11c54ac06d109dd
EunJu JO <dmswnehf@naver.com>
2023-09-24 04:41:18 +0900

    feat : 상품 컴포넌트 퍼블리싱 및 드롭다운 구현

--- src/components/ProductForm/index.tsx
+++ src/components/ProductForm/index.tsx
@@ -1,4 +1,8 @@
-import styled from 'styled-components';
+import Heart from '../../assets/grayHeartIcon.svg'
+import Chat from '../../assets/chatIcon.svg'
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import * as S from './styles';
 
 interface Product {
   title: string;
@@ -11,57 +15,51 @@ interface ProductProps {
 }
 
 const ProductForm: React.FC<ProductProps> = ({ items }) => {
+  const location = useLocation();
+  const isSalsePage = location.pathname === '/saleslist';
+
+  const [dropDown, setDropDown] = useState(false);
+
   return (
-    <MainDiv>
-      <ProductList>
+    <S.Div>
         {items.map((item, index) => (
-          <ProductItem key={index}>
-            <ImageDiv style={{ backgroundImage: `url(${item.image_url_1})` }} />
-            <TextDiv>
-              <TitleDiv>{item.title}</TitleDiv>
-              <PriceDiv>{item.price}원</PriceDiv>
-            </TextDiv>
-            </ProductItem>
+        <S.Container>
+            <S.ProductContent>
+                <S.ImageDiv style={{ backgroundImage: `url(${item.image_url_1})` }} />
+                <S.TextDiv>
+                    <div>
+                        <S.TitleDiv>{item.title}</S.TitleDiv>
+                        <S.InfoDiv>
+                            <S.InfoContent>(이름)</S.InfoContent> <S.InfoContent>3일전</S.InfoContent>
+                        </S.InfoDiv>
+                    </div>
+                    <S.PriceDiv>{Number(item.price).toLocaleString()}원</S.PriceDiv>
+                </S.TextDiv>
+
+                <S.Section>
+                    <S.Part>
+                        <S.Image style={{backgroundImage: `url(${Heart})`}}/>
+                        <S.Value>4</S.Value>
+                    </S.Part>
+                    <S.Part>
+                        <S.Image style={{backgroundImage: `url(${Chat})`}} />
+                        <S.Value>5</S.Value>
+                    </S.Part>
+                    {/* 판매 내역 페이지일 경우에만 보이도록 함 */}
+                    {isSalsePage && <S.MenuBar onClick={() => {setDropDown(!dropDown)}}><S.Circle/></S.MenuBar>}
+                    {dropDown && 
+                        <S.Dropdown>
+                            <S.DropdownItem>거래 완료로 변경</S.DropdownItem>
+                            <S.DropdownItem>게시글 수정</S.DropdownItem>
+                            <S.DropdownItem>삭제</S.DropdownItem>
+                        </S.Dropdown>
+                    }
+                </S.Section>
+            </S.ProductContent>
+        </S.Container>
         ))}
-      </ProductList>
-    </MainDiv>
+    </S.Div>
   );
 };
-
-const MainDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ProductList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
-  gap: 10px;
-  max-width: 1080px;
-  margin: 0 auto;
-`;
-
-const ProductItem = styled.div`
-  margin-right: 10px;
-  margin-bottom: 15px;
-`;
-
-const ImageDiv = styled.div`
-  width: 100%;
-  height: 26.5rem;
-  background-size: cover;
-  cursor: pointer;
-`;
-
-const TextDiv = styled.div`
-  font-style: normal;
-  font-weight: 700;
-  font-size: 2.4rem;
-  line-height: 29px;
-`;
-
-const TitleDiv = styled.div``;
-const PriceDiv = styled.div``;
 
 export default ProductForm;
