ac559061dc68cd05c4b4bf08abcfcc8022268028
EunJu JO <dmswnehf@naver.com>
2023-10-27 23:16:23 +0900

    feat: 좋아요 목록 무한스크롤 적용 중

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,71 +1,71 @@
-import { rest } from 'msw';
-import { CategoryData, CategoryNumData } from '@/types/category';
-import { mainCategory } from '@/constants/mainCategory';
+// import { rest } from 'msw';
+// import { CategoryData, CategoryNumData } from '@/types/category';
+// import { mainCategory } from '@/constants/mainCategory';
 
-const dummy = '테스트데이터';
-const category: CategoryData[] = [
-  {
-    id: 1,
-    title: '디지털기기',
-    image: 'category1',
-  },
-  {
-    id: 2,
-    title: '여성의류',
-    image: 'category2',
-  },
-  {
-    id: 3,
-    title: '남성의류/잡화',
-    image: 'category3',
-  },
-  {
-    id: 4,
-    title: '뷰티/미용',
-    image: 'category4',
-  },
-  {
-    id: 5,
-    title: '여성잡화',
-    image: 'category5',
-  },
-  {
-    id: 6,
-    title: '생활가전',
-    image: 'category6',
-  },
-  {
-    id: 7,
-    title: '생활/주방',
-    image: 'category7',
-  },
-  {
-    id: 8,
-    title: '취미/게임/음반',
-    image: 'category8',
-  },
-  {
-    id: 9,
-    title: '도서',
-    image: 'category9',
-  },
-];
+// const dummy = '테스트데이터';
+// const category: CategoryData[] = [
+//   {
+//     id: 1,
+//     title: '디지털기기',
+//     image: 'category1',
+//   },
+//   {
+//     id: 2,
+//     title: '여성의류',
+//     image: 'category2',
+//   },
+//   {
+//     id: 3,
+//     title: '남성의류/잡화',
+//     image: 'category3',
+//   },
+//   {
+//     id: 4,
+//     title: '뷰티/미용',
+//     image: 'category4',
+//   },
+//   {
+//     id: 5,
+//     title: '여성잡화',
+//     image: 'category5',
+//   },
+//   {
+//     id: 6,
+//     title: '생활가전',
+//     image: 'category6',
+//   },
+//   {
+//     id: 7,
+//     title: '생활/주방',
+//     image: 'category7',
+//   },
+//   {
+//     id: 8,
+//     title: '취미/게임/음반',
+//     image: 'category8',
+//   },
+//   {
+//     id: 9,
+//     title: '도서',
+//     image: 'category9',
+//   },
+// ];
 
-export const handlers = [
-  rest.get(`/category/:id=${category.id}`, async (req, res, ctx) => {
-    const categoryId = parseInt(req.params.category.id);
-    const filterCategory = category.find((categoryId) => categoryId.id === mainCategory);
-    if (filterCategory) {
-      return res(ctx.status(200), ctx.json(filterCategory));
-    } else {
-      return res(ctx.status(404), ctx.json({ message: '카테고리 못찾겠어여' }));
-    }
-  }),
-];
+// export const handlers = [
+//   rest.get(`/category/:id=${category.id}`, async (req, res, ctx) => {
+//     const categoryId = parseInt(req.params.category.id);
+//     const filterCategory = category.find((categoryId) => categoryId.id === mainCategory);
+//     if (filterCategory) {
+//       return res(ctx.status(200), ctx.json(filterCategory));
+//     } else {
+//       return res(ctx.status(404), ctx.json({ message: '카테고리 못찾겠어여' }));
+//     }
+//   }),
+// ];
 
 // mock/api로 분리
-// import wishResultHandler from './api/wishResultHandler';
-// import salseResultHandler from './api/salseResultHandler';
-// import purchaseResultHandler from './api/purchaseResultHandler';
+import wishResultHandler from './api/wishResultHandler';
+import salseResultHandler from './api/salseResultHandler';
+import purchaseResultHandler from './api/purchaseResultHandler';
 
-// export const handlers = [...wishResultHandler, ...salseResultHandler, ...purchaseResultHandler];
+export const handlers = [...wishResultHandler, ...salseResultHandler, ...purchaseResultHandler];
--- src/pages/WishList/index.tsx
+++ src/pages/WishList/index.tsx
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import ProductForm from '@/components/ProductForm';
 import TopNavBar from '@/components/TopNavBar';
 import * as S from './styles';
 import { Product } from '@/types/product';
 import { BASE_URL } from '@/constants/baseURL';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 
 const WishList: React.FC = () => {
   const [items, setItems] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const idx = useRef<number>(1);
 
   // 좋아요를 누른 상품 리스트 불러오는 함수
   const fetchWishList = async () => {
-    setIsLoading(true);
     try {
-      const response = await axios.get(`${BASE_URL}/mypage/like`);
+      const response = await axios.get(`${BASE_URL}/mypage/like`, {
+        params: {pageNo: idx, pageSize: 10}, // pageNo → 1부터 시작 , pageSize → 한페이지에 들어가는 게시물
+      });
+
       if (response.data) {
         // 각 상품마다 채팅방 개수 가져오는 Promise 배열 생성
         const chatroomCountPromises = response.data.map((product: Product) =>
@@ -28,13 +32,17 @@ const WishList: React.FC = () => {
           chatroomCount: chatroomCounts[index].data,
         }));
 
-        setItems(updatedProducts);
+        // setItems(updatedProducts);
+        // idx.current++;
+        setItems(prevItems => [...prevItems, ...updatedProducts]);
       }
     } catch (error) {
       console.error(error);
     }
     setIsLoading(false);
   };
+
+  const { isFetching } = useInfiniteScroll(fetchWishList);
 
   useEffect(() => {
     fetchWishList();
@@ -45,11 +53,12 @@ const WishList: React.FC = () => {
       <TopNavBar page="좋아요 목록" />
 
       <S.ProductContainer>
-        {isLoading ? (
+        {isLoading && !isFetching ? (
           <div>로딩 중...</div> // 로딩중 뷰 추가 예정
         ) : (
           <ProductForm items={items} refreshProductList={fetchWishList} />
         )}
+        {isFetching && <div>추가 항목 로딩 중...</div>}
       </S.ProductContainer>
     </>
   );
