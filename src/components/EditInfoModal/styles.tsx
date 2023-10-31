61f8ef91024ddf43fa04b803acd1e598c9b31e10
EunJu JO <dmswnehf@naver.com>
2023-11-01 01:43:09 +0900

    fix: 정보 변경 모달창 스타일 수정

--- src/components/EditInfoModal/styles.tsx
+++ src/components/EditInfoModal/styles.tsx
@@ -1,49 +1,59 @@
 import styled from 'styled-components';
 
 export const BtnArea = styled.button`
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    width: 3rem;
-    height: 3rem;
-    appearance: none;
-    border: 0;
-    padding: 0;
-    background-color: transparent;
-    &:hover {
-        cursor: pointer;
-        color: #828385;
-    }
-`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 3rem;
+  height: 3rem;
+  appearance: none;
+  border: 0;
+  padding: 0;
+  background-color: transparent;
+  &:hover {
+    cursor: pointer;
+    color: #828385;
+  }
+`;
 
 export const Contaniner = styled.div`
-    padding-bottom: 20px;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 2rem;
+`;
 
 export const Label = styled.span`
-    padding-right: 14px;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 700;
-`
+  padding-right: 1.4rem;
+  font-size: 1.4rem;
+  color: #000000;
+  font-weight: 700;
+`;
 
 export const InputBox = styled.input`
-    appearance: none;
-    border: 0;
-    padding: 0;
-    background-color: transparent;
-    border-bottom: 1px solid #D9D9D9;
-    font-size: 14px;
-    color: #828385;
-`
+  height: 2.4rem;
+  padding: 0 1rem;
+  appearance: none;
+  border: none;
+  background-color: #efefef;
+  border-radius: 1rem;
+  font-size: 1.4rem;
+`;
 
 export const EditBtn = styled.button`
-    width: 60px;
-    height: 24px;
-    font-size: 12px;
-    appearance: none;
-    border: 0;
-    border-radius: 10px;
-    background-color: #FD8944;
-    color: #FFF;
-`
+  position: absolute;
+  bottom: 2rem;
+  width: 6rem;
+  height: 2.4rem;
+  font-size: 1.2rem;
+  font-weight: 500;
+  appearance: none;
+  border: 0;
+  border-radius: 10px;
+  background-color: #fd8944;
+  color: #fff;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
