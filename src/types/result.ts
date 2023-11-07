60141e4c58d0948ccf04724c73e5ffa890351acd
yura <yuraa0302@gmail.com>
2023-11-08 01:41:28 +0900

    fix : wishResultData, result타입추가

---
+++ src/types/result.ts
@@ -0,0 +1,25 @@
+// export type ResultData = {
+
+//   productUuid: string;
+//   title: string;
+//   price: number;
+//   image_url_1: string;
+//   productState: 'SALE' | 'RESERVED' | 'SOLD'; // 판매중, 예약중, 판매완료
+//   tradeType: 'CoolDeal' | 'GeneralDeal';
+//   likeCount: number; // 좋아요 개수
+//   chatroomCount: number; // 채팅방 개수
+// };
+
+export type Product = {
+  productUuid: string;
+  title: string;
+  name: string;
+  date: string;
+  price: number;
+  image_url_1: string;
+  productState: 'SALE' | 'RESERVED' | 'SOLD'; // 판매중, 예약중, 판매완료
+  likeCount: number; // 좋아요 개수
+  userHasLiked: boolean; // 사용자가 좋아요 누른지 여부
+  chatroomCount: number; // 채팅방 개수
+};
+
