c4c5db84897337cf49e049a70e5da230c4d4b54b
yura <yuraa0302@gmail.com>
2023-11-17 00:53:34 +0900

    fix : input 별 type 추가

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -15,12 +15,15 @@ const index = () => {
       ></img>
       <S.Inputs>
         <S.NameInput placeholder="이름"></S.NameInput>
-        <S.EmailInput placeholder="이메일"></S.EmailInput>
-        <S.PasswordInput placeholder="비밀번호"></S.PasswordInput>
-        <S.PasswordCorrectInput placeholder="비밀번호 확인"></S.PasswordCorrectInput>
+        <S.EmailInput type="email" placeholder="이메일"></S.EmailInput>
+        <S.PasswordInput type="password" placeholder="비밀번호"></S.PasswordInput>
+        <S.PasswordCorrectInput
+          type="password"
+          placeholder="비밀번호 확인"
+        ></S.PasswordCorrectInput>
       </S.Inputs>
       <S.Buttons>
-        <S.SaveBtn>저장하기</S.SaveBtn>
+        <S.SaveBtn type="submit">저장하기</S.SaveBtn>
         <S.CancelBtn>취소하기</S.CancelBtn>
       </S.Buttons>
     </S.Container>
