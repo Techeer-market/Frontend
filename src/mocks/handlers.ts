af04f8bf3c74a8abd0473ab7f23c9be614df1dbb
EunJu JO <dmswnehf@naver.com>
2023-12-27 01:49:23 +0900

    fix: mock handler 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,16 +1,3 @@
-// export const handlers = [
-//   rest.get(`/category/:id=${category.id}`, async (req, res, ctx) => {
-//     const categoryId = parseInt(req.params.category.id);
-//     const filterCategory = category.find((categoryId) => categoryId.id === mainCategory);
-//     if (filterCategory) {
-//       return res(ctx.status(200), ctx.json(filterCategory));
-//     } else {
-//       return res(ctx.status(404), ctx.json({ message: '카테고리 못찾겠어여' }));
-//     }
-//   }),
-// ];
-
-// mock/api로 분리
 import mainResultHandler from './api/mainResultHandler';
 import wishResultHandler from './api/wishResultHandler';
 import salseResultHandler from './api/salseResultHandler';
