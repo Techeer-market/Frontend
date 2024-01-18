9411be632978d679913767c671bcd1fa02c90c52
yura <yuraa0302@gmail.com>
2024-01-19 01:57:47 +0900

    fix : searchPage 로직 수정 및 Router.tsx 경로 수정

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -76,6 +76,7 @@ export const restFetcher = async ({
     };
     if (body) axiosConfig.data = body;
     if (params) {
+      console.log(params);
       const searchParams = new URLSearchParams(params);
       url += '?' + searchParams.toString();
     }
