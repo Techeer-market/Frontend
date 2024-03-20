b81891b1dac5d7b16a4c3f0009d95b4a3f0c2e86
yura <yuraa0302@gmail.com>
2024-03-21 02:12:01 +0900

    design : 컨테이너 스타일 수정

--- src/components/ChatContainer/styles.tsx
+++ src/components/ChatContainer/styles.tsx
@@ -1,4 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-export const TodayDate = styled.div``;
+export const Container = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  width: 98%;
+  height: 90%;
+  margin: 0 auto;
+  overflow-y: scroll;
+`;
+export const TodayDate = styled.div`
+  width: 336px;
+  height: 25px;
+  background-color: #717171;
+  margin: 20px 0;
+  border-radius: 10px;
+  text-align: center;
+  line-height: 25px;
+  color: white;
+  font-size: 15px;
+`;
